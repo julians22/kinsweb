b7d4c1ffb00f42063a9638b3a2cb5b831dd08fa7
julians22 <dabnerjulian@gmail.com>
2025-04-13 19:01:41 +0700

    feat: add swiper dependency and update app layout for better responsiveness

--- app.vue
+++ app.vue
@@ -1,23 +1,26 @@
 <template>
-  <div class="flex items-center bg-white dark:bg-gray-900 w-full h-svh">
+  <div class="flex items-center bg-white dark:bg-gray-900 w-full h-svh overflow-auto">
     
-    <div class="mx-auto px-4 w-full max-w-md">
+    <div class="mx-auto px-4 w-full max-w-md h-full">
 
       <!-- Centered page with backgorund image -->
-      <div class="flex flex-col justify-center items-center bg-cover bg-center p-5 h-screen" style="background-image: url('/img/background.png');">
+      <div class="flex flex-col items-center gap-5 bg-cover bg-center p-5 h-full overflow-auto" style="background-image: url('/img/background.png');">
 
         <!-- card R5 -->
          <div class="flex flex-col gap-3 bg-white/80 dark:bg-gray-800/80 shadow-md p-6 rounded-3xl w-full max-w-sm">
+           
+           <h1 class="font-black text-primary dark:text-white text-6xl text-center">OUR R5</h1>
+           <img src="/img/Unclee Bear.png" alt="">
+           <h1 class="font-black text-primary dark:text-white text-6xl text-center">Unclee Bear</h1>
+           
+          </div>
           
-          <h1 class="font-black text-primary dark:text-white text-6xl text-center">OUR R5</h1>
-          <img src="/img/Unclee Bear.png" alt="">
-          <h1 class="font-black text-primary dark:text-white text-6xl text-center">Unclee Bear</h1>
-
-         </div>
-
-
-         <!-- card r4 -->
           
+          <!-- card r4 -->
+          <div class="flex flex-col gap-3 bg-white/80 dark:bg-gray-800/80 shadow-md p-6 rounded-3xl w-full max-w-sm">
+            <h1 class="font-black text-primary dark:text-white text-6xl text-center">OUR R4</h1>
+            
+          </div>
 
       </div> 
 
