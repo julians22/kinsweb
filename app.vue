f6bd5a2e1ec6b96a5033cfdefef384918317467c
julians22 <dabnerjulian@gmail.com>
2025-04-13 18:53:55 +0700

    feat: initialize Nuxt.js application with essential configurations and assets
    - Added package.json with scripts and dependencies for Nuxt.js and Tailwind CSS
    - Included favicon and various image assets in the public/img directory
    - Created robots.txt for SEO purposes
    - Set up TypeScript configuration files for server and project

---
+++ app.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex items-center bg-white dark:bg-gray-900 w-full h-svh">
+    
+    <div class="mx-auto px-4 w-full max-w-md">
+
+      <!-- Centered page with backgorund image -->
+      <div class="flex flex-col justify-center items-center bg-cover bg-center p-5 h-screen" style="background-image: url('/img/background.png');">
+
+        <!-- card R5 -->
+         <div class="flex flex-col gap-3 bg-white/80 dark:bg-gray-800/80 shadow-md p-6 rounded-3xl w-full max-w-sm">
+          
+          <h1 class="font-black text-primary dark:text-white text-6xl text-center">OUR R5</h1>
+          <img src="/img/Unclee Bear.png" alt="">
+          <h1 class="font-black text-primary dark:text-white text-6xl text-center">Unclee Bear</h1>
+
+         </div>
+
+
+         <!-- card r4 -->
+          
+
+      </div> 
+
+    </div>
+
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,22 @@
+import tailwindcss from "@tailwindcss/vite";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  modules: [
+    [
+      '@nuxtjs/google-fonts', {
+        families: {
+          'League Gothic': true,
+        },
+      }
+    ]
+  ],
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+})
